9c7a9761c189465dee1b88dbadad0d2cbd95f15c
nikita <marakaci1999@gmail.com>
2018-02-24 14:09:46 +0200

    Switch to sqllite in dev

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,26 +90,26 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if 'DB_NAME' in os.environ:
-    # Running the Docker image
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ['DB_NAME'],
-            'USER': os.environ['DB_USER'],
-            'PASSWORD': os.environ['DB_PASS'],
-            'HOST': os.environ['DB_SERVICE'],
-            'PORT': os.environ['DB_PORT']
-        }
-    }
-else:
+# if 'DB_NAME' in os.environ:
+#     # Running the Docker image
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': os.environ['DB_NAME'],
+#             'USER': os.environ['DB_USER'],
+#             'PASSWORD': os.environ['DB_PASS'],
+#             'HOST': os.environ['DB_SERVICE'],
+#             'PORT': os.environ['DB_PORT']
+#         }
+#     }
+# else:
     # Building the Docker image
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
