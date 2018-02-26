a470ef06d819e2b8b30d3e9b30c81492fc0e25d2
nikita <mykyta.chernenko@nure.ua>
2018-02-26 14:13:40 +0200

    refresh jwt token functionality

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 from users.views import UserListView, UserDetailView, SelfUserDetailView, FriendListView, activate_account
 
 urlpatterns = [
-        path('users/', UserListView.as_view(), name='user-list'),
-        path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-        path('self-users/', SelfUserDetailView.as_view(), name='self-user-detail'),
-        path('friends/', FriendListView.as_view(), name='friend-list'),
-        path('activate-account/<str:activate_link>/', activate_account, name='activate-account'),
-        path('obtain-jwt-token/', obtain_jwt_token, name='obtain-jwt-for-email-username-phone')
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
+    path('self-users/', SelfUserDetailView.as_view(), name='self-user-detail'),
+    path('friends/', FriendListView.as_view(), name='friend-list'),
+    path('activate-account/<str:activate_link>/', activate_account, name='activate-account'),
+    path('obtain-jwt-token/', obtain_jwt_token, name='obtain-jwt-token'),
+    path('refresh-jwt-token/', refresh_jwt_token, name='refresh-jwt-token')
 ]
